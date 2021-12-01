ffb537b105e8223d172d9d7af253bfa5913f4661
Dmitry Klepikov <dmitry@pixoutserver.com>
2021-12-01 18:18:27 +0200

    Format improving
    Improve output format
    Add usage and flag for outputting to the file

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"os"
 	"runtime"
 	"time"
-        "flag"
 
 	"github.com/boguslaw-wojcik/crc32a"
 	"github.com/jsimonetti/go-artnet/packet"
 	"github.com/jsimonetti/go-artnet/packet/code"
 
-	"./pkg/stat"
+	"github.com/pixout/artnet-analyzer/pkg/stat"
 )
 
 type message struct {
@@ -24,28 +25,32 @@ type message struct {
 type messageQueue chan message
 
 var mq messageQueue
-var stat artanalyzer.Stat
+var stat *artanalyzer.Stat
+var output string
 
 func (mq messageQueue) enqueue(m message) {
 	mq <- m
 }
 
 func list_duration() {
-	for m := range mq {
 
-		stat.Total_ms = stat.Total_ms + m.delay.Truncate(time.Millisecond)
-		stat.Total_ns = stat.Total_ns + m.delay
-		stat.Total_packets = stat.Total_packets + 1
+	templ_format1 := "#\t%010d\tTimeCode\t%v\tOP\t%s\tDT\t%010v\tTM1\t%010v\tTM2\t%010v"
+	templ_format2 := "\tUni\t%03d\t%03d\t%03d\tSeq\t%03d\tCRC32\t%08x"
+	file, err := os.Create(output)
+	fmt.Printf("Worker started waiting for the data\n")
+
+	if err != nil {
+		fmt.Printf("Error reading output file %s due to: %v ", output, err)
+		return
+	}
+
+	for m := range mq {
 
 		artp, err := packet.Unmarshal(m.msg)
 		if err != nil {
 
-			fmt.Printf("ArtNet decode error %v ", err)
-		} else {
-
-			op := artp.GetOpCode()
-
-			fmt.Printf("ArtNet: OpCode %s ", op.String())
+			fmt.Printf("Error in ArtNet packet unmarshal due to: %v ", err)
+			continue
 		}
 
 		if artp.GetOpCode() == code.OpOutput {
@@ -55,34 +60,49 @@ func list_duration() {
 			dmx.UnmarshalBinary(data)
 			if err != nil {
 
-				fmt.Printf("ArtNet: Uni erroor %v ", err)
-			} else {
-
-				fmt.Printf("ArtNet: Uni %d-%d-%d,%d ", dmx.Net, dmx.SubUni, dmx.Physical, dmx.Sequence)
+				fmt.Printf("Error in ArtNet DMX unmarshal due to: %v ", err)
+				continue
 			}
+
+			fmt.Fprintf(file, templ_format1+templ_format2+"\n",
+				stat.Total_packets,
+				time.Now().UnixNano(),
+				"DMX",
+				m.delay,
+				stat.Total_ms,
+				stat.Total_ns,
+				dmx.Net,
+				dmx.SubUni,
+				dmx.Physical,
+				dmx.Sequence,
+				crc32a.Checksum(m.msg))
+
+		} else {
+
+			op := artp.GetOpCode()
+			fmt.Fprintf(file, templ_format1+"\n",
+				stat.Total_packets,
+				time.Now().UnixNano(),
+				op.String(),
+				m.delay,
+				stat.Total_ms,
+				stat.Total_ns)
+
 		}
 
-		fmt.Printf("delay %v ", m.delay)
-		fmt.Printf("CRC32: %08x, timecode %v\n", crc32a.Checksum(m.msg), time.Now().UnixNano())
-
-		//            fmt.Printf("time: %v\n", m)
-	}
-}
-
-func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
-	_, err := conn.WriteToUDP([]byte("From server: Hello I got your message "), addr)
-	if err != nil {
-		fmt.Printf("Couldn't send response %v", err)
+		stat.Total_ms = stat.Total_ms + m.delay.Truncate(time.Millisecond)
+		stat.Total_ns = stat.Total_ns + m.delay
+		stat.Total_packets = stat.Total_packets + 1
 	}
 }
 
 func init() {
-	//	flag.BoolVar(&useMs, "ms", false, "Use miliseconds if true")
+	flag.StringVar(&output, "output", "output.tsv", "Output file for result")
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	p := make([]byte, 2048)
+
 	addr := net.UDPAddr{
 		Port: 6454,
 		IP:   nil,
@@ -90,8 +110,10 @@ func main() {
 
 	flag.Parse()
 
-	stat := artanalyzer.NewStat()
+	fmt.Printf("ArtNet Analyzer listening on the port: %v\n", addr.Port)
+	fmt.Printf("Output file: %s\n", output)
 
+	stat = artanalyzer.NewStat()
 	mq = make(messageQueue, 100)
 	go list_duration()
 
@@ -101,24 +123,29 @@ func main() {
 
 			if stat.Total_packets > 0 {
 
-				fmt.Printf("total time: ms %v, ns %v  (diff %v), packets amount %v, avg: %v\n", stat.Total_ms, stat.Total_ns, stat.Total_ns-stat.Total_ms, stat.Total_packets, time.Duration(int(stat.Total_ns)/stat.Total_packets))
+				fmt.Printf("Statistics!\tTotal in (ms %010v, ns %010v) = (diff %010v), packets amount %010v, avg: %010v\n", stat.Total_ms, stat.Total_ns, stat.Total_ns-stat.Total_ms, stat.Total_packets, time.Duration(int(stat.Total_ns)/stat.Total_packets))
 			}
 		}
 	}()
 
-	fmt.Printf("Server listening on the port: %v\n", addr.Port)
-
 	ser, err := net.ListenUDP("udp", &addr)
 	if err != nil {
-		fmt.Printf("Some error %v\n", err)
+		fmt.Printf("Error listen port due to: %v\n", err)
 		return
 	}
 
 	defer ser.Close()
 	var prev time.Time
+	p := make([]byte, 2048)
 
 	for {
 		nbytes, remoteaddr, err := ser.ReadFromUDP(p)
+
+		if err != nil {
+			fmt.Printf("Error Read UDP from addr %v due to: %v\n", remoteaddr, err)
+			prev = time.Now()
+			continue
+		}
 
 		if prev.IsZero() {
 			prev = time.Now()
@@ -131,11 +158,5 @@ func main() {
 		}
 
 		prev = time.Now()
-
-		//fmt.Printf("Read a message from: %s, %v, size: %d \n", remoteaddr, p[0:num], num)
-		if err != nil {
-			fmt.Printf("Some error: addr  %v, err %v, data: %v", remoteaddr, err, p)
-			continue
-		}
 	}
 }
